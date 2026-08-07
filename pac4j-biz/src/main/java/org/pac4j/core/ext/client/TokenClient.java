8b960848b2bdcb55eabb684b8ae5d1e66699f63d
wandl-6A72h <hiwepy@gmail.com>
2026-08-07 23:34:00 +0800

    refactor: consolidate pac4j modules

---
+++ pac4j-biz/src/main/java/org/pac4j/core/ext/client/TokenClient.java
@@ -0,0 +1,107 @@
+/*
+ * Copyright (c) 2018, Loong Wan (https://github.com/loong10k).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.pac4j.core.ext.client;
+
+import org.pac4j.core.client.DirectClient;
+import org.pac4j.core.ext.credentials.authenticator.TokenAuthenticator;
+import org.pac4j.core.ext.credentials.extractor.TokenParameterExtractor;
+import org.pac4j.core.ext.profile.Token;
+import org.pac4j.core.ext.profile.TokenProfile;
+import org.pac4j.core.ext.profile.creator.TokenProfileCreator;
+import org.pac4j.core.profile.creator.ProfileCreator;
+import org.pac4j.core.util.CommonHelper;
+
+public abstract class TokenClient<P extends TokenProfile, T extends Token> extends DirectClient {
+
+    private String parameterName = "";
+
+
+    private boolean supportGetRequest = false;
+
+
+    private boolean supportPostRequest = true;
+    /**
+
+     * The location of the client login URL, i.e. https://localhost:8080/myapp/login
+
+     */
+    private String loginUrl;
+
+
+    public TokenClient() {
+    }
+
+    public TokenClient(final String parameterName, final TokenAuthenticator<P, T> tokenAuthenticator) {
+        this.parameterName = parameterName;
+        setAuthenticatorIfUndefined(tokenAuthenticator);
+    }
+
+    public TokenClient(final String parameterName,
+                           final TokenAuthenticator<P, T> tokenAuthenticator,
+                           final ProfileCreator profileCreator) {
+        this.parameterName = parameterName;
+        setAuthenticatorIfUndefined(tokenAuthenticator);
+        setProfileCreatorIfUndefined(profileCreator);
+    }
+
+
+    @Override
+    protected void internalInit(boolean forceReinit) {
+        setProfileCreatorIfUndefined(new TokenProfileCreator());
+        setCredentialsExtractorIfUndefined(new TokenParameterExtractor(this.getParameterName(), this.isSupportGetRequest(), this.isSupportPostRequest()));
+
+
+        // ensures components have been properly initialized
+        CommonHelper.assertNotNull("credentialsExtractor", getCredentialsExtractor());
+        CommonHelper.assertNotNull("authenticator", getAuthenticator());
+        CommonHelper.assertNotNull("profileCreator", getProfileCreator());
+    }
+
+
+    public String getParameterName() {
+        return parameterName;
+    }
+
+    public void setParameterName(String parameterName) {
+        this.parameterName = parameterName;
+    }
+
+    public boolean isSupportGetRequest() {
+        return supportGetRequest;
+    }
+
+    public void setSupportGetRequest(boolean supportGetRequest) {
+        this.supportGetRequest = supportGetRequest;
+    }
+
+    public boolean isSupportPostRequest() {
+        return supportPostRequest;
+    }
+
+    public void setSupportPostRequest(boolean supportPostRequest) {
+        this.supportPostRequest = supportPostRequest;
+    }
+
+
+    public void setLoginUrl(String loginUrl) {
+        this.loginUrl = loginUrl;
+    }
+
+
+    public String getLoginUrl() {
+        return loginUrl;
+    }
+}
---
+++ pac4j-biz/src/main/java/org/pac4j/core/ext/credentials/UsernamePasswordCaptchaCredentials.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright (c) 2018, Loong Wan (https://github.com/loong10k).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.pac4j.core.ext.credentials;
+
+import org.pac4j.core.credentials.Credentials;
+import org.pac4j.core.ext.Pac4jExtConstants;
+import org.pac4j.core.util.Pac4jConstants;
+
+/**
+ * TODO
+ * @author [@Loong Wan](https://github.com/loong10k)
+ */
+@SuppressWarnings("serial")
+public class UsernamePasswordCaptchaCredentials extends Credentials {
+
+    private String username;
+
+    private String password;
+
+
+    private String captcha;
+
+    public UsernamePasswordCaptchaCredentials(final String username, final String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+
+    public UsernamePasswordCaptchaCredentials(final String username, final String password, String captcha) {
+        this.username = username;
+        this.password = password;
+        this.captcha = captcha;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public String getCaptcha() {
+        return captcha;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        final UsernamePasswordCaptchaCredentials that = (UsernamePasswordCaptchaCredentials) o;
+
+        if (username != null ? !username.equals(that.getUsername()) : that.getUsername() != null) return false;
+        if (password != null ? !password.equals(that.getPassword()) : that.getPassword() != null) return false;
+        return !(captcha != null ? !captcha.equals(that.getCaptcha()) : that.getCaptcha() != null);
+    }
+
+
+    @Override
+    public int hashCode() {
+        int result = username != null ? username.hashCode() : 0;
+        result = 31 * result + (password != null ? password.hashCode() : 0);
+        result = 31 * result + (captcha != null ? captcha.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + "{" + Pac4jConstants.USERNAME + "='" + username + "', "
+                + Pac4jConstants.PASSWORD + "='[PROTECTED]', " + Pac4jExtConstants.CAPTCHA + "='[PROTECTED]'}";
+    }
+}
